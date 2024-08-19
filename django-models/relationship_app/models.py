f2e8a939fa7d4687db7489c0685da646ac5359ef
timon-yego <kipyegotimothy99@gmail.com>
2024-08-19 21:24:02 +0300

    modified Implementing Advanced Model Relationships in Django

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -6,10 +6,16 @@ from django.dispatch import receiver
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
+     
+    def __str__(self):
+         return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         permissions = [
@@ -21,10 +27,16 @@ class Book(models.Model):
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
-
+    
+    def __str__(self):
+        return self.name
+    
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
    
 class UserProfile(models.Model):
     ROLE_CHOICES = [
